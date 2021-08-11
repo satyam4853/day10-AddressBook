46295a1892740620221e8c8302b89032c7f2a637
Your Name <you@example.com>
2021-08-11 16:40:49 +0530

    Refactor : UC1-UC4

---
+++ AddressBook-Opps/AddressBookDetails.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook_Opps
+{
+    class AddressBookDetails
+    {
+
+        private static List<Person> contacts = new List<Person>();
+        public static void AddMember()
+        {
+            //object for person class
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            person.lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            person.address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            person.city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            person.state = Console.ReadLine();
+            Console.Write("Enter Zip Code: ");
+            person.zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Email : ");
+            person.email = Console.ReadLine();
+            Console.Write("Enter Phone Number : ");
+            person.phoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            contacts.Add(person);
+
+            Console.WriteLine("Successfully Added");
+        }
+
+        public static void ViewContacts()
+        {
+            if (contacts.Count > 0)
+            {
+                Console.WriteLine("/t /t /t Your Contact List Has");
+                foreach (var x in contacts)
+                {
+                    PrintValues(x);
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Address Book is Empty");
+            }
+        }
+
+        public static void PrintValues(Person x)
+        {
+            Console.WriteLine($"First Name : {x.firstName}");
+            Console.WriteLine($"Last Name : {x.lastName}");
+            Console.WriteLine($"Address : {x.address}");
+            Console.WriteLine($"City : {x.city}");
+            Console.WriteLine($"State : {x.state}");
+            Console.WriteLine($"Zip Code: {x.zipCode}");
+            Console.WriteLine($"Phone Number: {x.phoneNumber}");
+            Console.WriteLine($"Email: {x.email}");
+
+        }
+        public static void EditDetails()
+        {
+            int f;//flag variable
+            if (contacts.Count > 0)
+            {
+                Console.Write("Enter name of a person you want to edit: ");
+                string editName = Console.ReadLine();
+
+                foreach (var x in contacts)
+                {
+                    if (editName.ToLower() == x.firstName.ToLower())
+                    {
+                        while (true)
+                        {
+                            f = 0;
+                            Console.WriteLine("1.First name\n2.Last name\n3.Address\n4.City\n5.State\n6.ZipCode\n7.Phone Number\n8.email\n9.Exit");
+                            Console.WriteLine("Enter Option You want to edit");
+                            switch (Convert.ToInt32(Console.ReadLine()))
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter New First name");
+                                    x.firstName = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter New Last name");
+                                    x.lastName = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter New Address");
+                                    x.address = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter New City");
+                                    x.city = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter New State");
+                                    x.state = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter New Zip Code");
+                                    x.zipCode = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 7:
+                                    Console.Write("Enter new Phone Number: ");
+                                    x.phoneNumber = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+
+                                case 8:
+                                    Console.Write("Enter new Email-id: ");
+                                    x.email = Console.ReadLine();
+                                    Console.WriteLine("\t \t \t MODIFIED");
+                                    break;
+                                case 9:
+                                    // to exit from main method
+                                    Console.WriteLine("Exited");
+                                    f = 1;
+                                    break;
+
+                            }
+                            if (f == 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entered name is not in Contact list");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your contact list is empty");
+            }
+        }
+
+        //method for deleting conatcts
+        public static void DeleteDetails()
+        {
+            int f = 0;
+            if (contacts.Count > 0)
+            {
+                Console.Write("Enter name of a person you want to Delete: ");
+                string deleteName = Console.ReadLine();
+
+                foreach (var x in contacts)
+                {
+                    if (deleteName.ToLower() == x.firstName.ToLower())
+                    {
+                        //removing from list
+                        Console.WriteLine("\t \t \tDELETED");
+                        Console.WriteLine($"You have deleted {x.firstName} contact");
+                        contacts.Remove(x);
+                        f = 1;
+                        break;
+                    }
+                }
+                if (f == 0)
+                {
+                    Console.WriteLine("The name you have entered not in the address book");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Your contact list is empty");
+            }
+        }
+
+    }
+}
---
+++ AddressBook-Opps/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AddressBook_Opps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Welcome to AddressBook problem------------");
+            while (true)
+            {
+
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        AddressBookDetails.AddMember();
+                        break;
+                    case 2:
+                        AddressBookDetails.ViewContacts();
+                        break;
+                    case 3:
+                        AddressBookDetails.EditDetails();
+                        break;
+                    case 4:
+                        AddressBookDetails.DeleteDetails();
+                        break;
+                    case 5:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+
+
+            }
+        }
+    }
+}
